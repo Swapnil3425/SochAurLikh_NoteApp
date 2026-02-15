faf36f712d4439a2b1d24315d281cbfe1e54d760
Swapnil3425 <swapnilg8682@gmail.com>
2026-02-15 13:30:25 +0530

    Configure project for deployment

--- backend/index.js
+++ backend/index.js
@@ -874,8 +874,9 @@ app.put("/update-note-private/:noteId", authenticateToken, async (req, res) => {
 });
 
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
